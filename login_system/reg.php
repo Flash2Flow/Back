dcf7930502e6a59f5dc6719955f76cd49d9169ff
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-04-08 20:20:03 +1000

    Create reg.php

---
+++ login_system/reg.php
@@ -0,0 +1,48 @@
+<?php
+
+//include module
+require "./db/db.php";
+
+//main variable
+$data = $_POST;
+
+//if press button reg
+if( isset($data['submit_reg']) )
+{
+
+//errors variable list ( login + password )
+	$errors = array();
+	if( trim($data['login']) == '	' )
+	{
+		$errors[] = 'Invalid login';
+	}
+
+	if( ($data['password']) == '	' )
+	{
+		$errors[] = 'Invalid password';
+	}
+
+//cheak on unique login
+	if( R::count('users', "login = ?", array($data['login'])) > 0 )
+	{
+		$errors[] = 'User with this login already have';
+	}
+
+// if error not, create database. If database create, register complited
+	if( empty($errors))
+	{
+		$user = R::dispense('users');
+		$user->login = $data['login'];
+		$user->password = password_hash($data['password'], PASSWORD_DEFAULT);
+		R::store($user);
+		echo '<div style="color: green;">You are gay</div><hr>';
+	} else
+
+//if error, show error
+	{
+		echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
+	}
+}
+
+
+?>
