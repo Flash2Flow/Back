26fca56eaaa7bd30a6e6a543953888b2c1640f3a
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-04-08 20:03:32 +1000

    Update db.php

--- login_system/db/db.php
+++ login_system/db/db.php
@@ -1,7 +1,11 @@
 <?php
+//include module
 require "rb.php";
 
+//connect database
 R::setup( 'mysql:host=localhost;dbname=name',
 'user', 'pass' );
 
+
+//start session on full pages
 session_start();
