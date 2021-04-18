fd7a0042870c1a92c39369dca423a0088f3d695b
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-04-18 14:08:25 +1000

    Update main.php

--- check_on_admin/main.php
+++ check_on_admin/main.php
@@ -2,14 +2,19 @@
 require '../db/db.php';
 
 $user_active = $_SESSION['logged_user']->login;
+$user_active_group = $_SESSION['logged_user']->group;
+$user_admin = $_SESSION['logged_user']->admin;
 
-$user = R::findOne('users', 'dev = 1');
-if( $user->dev == 1 AND $user->login === $user_active )
-{
-echo 'You are admin';
-}
-else
-{
-echo 'You are not admin';
-}
+if( isset($_SESSION['logged_user'])) {
+    if( $user_admin == 1 ){
+      echo $user_active;
+    }
+    else{
+        header('Location: /');
+    }
+  }
+  else
+  {
+    header('Location: /');
+  }
 ?>
