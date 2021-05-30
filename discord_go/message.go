73bf33e4434a7e868b2d890fcb37ebf49bd5c16a
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-05-30 22:42:57 +1000

    Update message.go

--- discord_go/message.go
+++ discord_go/message.go
@@ -5,3 +5,9 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate){
     log.Println("Chat message: " + m.Content)
 }
 
+  func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate){
+		if m.Content == "req" {
+    _, _ = s.ChannelMessageSend(m.ChannelID, "res")
+		}
+}
+
